84993535b3573ff49d4c69ec30d36d1021610fc5
topen <80331849+slimalized@users.noreply.github.com>
2025-06-04 19:31:12 +0900

    feat: content collection の追加

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,7 +1,17 @@
 // @ts-check
 import { defineConfig } from "astro/config";
+import mdx from "@astrojs/mdx";
 
 // https://astro.build/config
 export default defineConfig({
 	site: "https://slimalized.dev",
+	integrations: [mdx()],
+	markdown: {
+		shikiConfig: {
+			themes: {
+				light: "github-light",
+				dark: "github-dark",
+			},
+		},
+	},
 });
