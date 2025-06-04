17c6542de51bb0359bdc9593c62bc25fe7dbcb79
topen <80331849+slimalized@users.noreply.github.com>
2025-06-04 19:36:31 +0900

    chore: react の追加

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,11 +1,12 @@
 // @ts-check
 import { defineConfig } from "astro/config";
 import mdx from "@astrojs/mdx";
+import react from "@astrojs/react";
 
 // https://astro.build/config
 export default defineConfig({
 	site: "https://slimalized.dev",
-	integrations: [mdx()],
+	integrations: [mdx(), react()],
 	markdown: {
 		shikiConfig: {
 			themes: {
