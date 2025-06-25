f54f405aad493ae31f9d8a08fa421773a4dbc3f5
topen <80331849+slimalized@users.noreply.github.com>
2025-06-25 12:03:29 +0900

    feat: post thumbnailの追加

--- src/utils/consts.ts
+++ src/utils/consts.ts
@@ -1,6 +1,8 @@
 export const site = {
 	domain: "slimalized.dev",
 	r2SubDomain: "media.slimalized.dev",
+	defaultThumbnailLink:
+		"https://media.slimalized.dev/posts/markdown-sample/icon.svg",
 	author: "slimalized",
 	description: "slimalizedの個人サイトです。散歩と寿司が大好き。",
 	lang: "ja",
