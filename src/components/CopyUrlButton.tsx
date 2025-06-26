21f09855a9d4649538d1efee6cf62b85d2e93842
topen <80331849+slimalized@users.noreply.github.com>
2025-06-26 22:02:21 +0900

    style: shareボタンのデザイン調整

--- src/components/CopyUrlButton.tsx
+++ src/components/CopyUrlButton.tsx
@@ -1,3 +1,4 @@
+import { Check, Copy } from "lucide-react";
 import { useCopy } from "../utils/hooks/useCopy";
 import styles from "./CopyUrlButton.module.css";
 
@@ -15,9 +16,15 @@ export const CopyUrlButton = ({ href }: { href: string }) => {
 					await copy(); // Copy url.
 				}}
 			>
-				<span data-is-copied={isCopied}>記事のURLをコピー</span>
-				<span data-is-copied={isCopied}>しました</span>
-				<span data-is-copied={isCopied}>する</span>
+				<span data-is-copied={isCopied}>URLをコピー</span>
+				<span data-is-copied={isCopied}>
+					しました
+					<Check />
+				</span>
+				<span data-is-copied={isCopied}>
+					する
+					<Copy />
+				</span>
 			</button>
 		</>
 	);
