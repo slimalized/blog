2584e90290304812809f082f2607c34119cd3df2
topen <80331849+slimalized@users.noreply.github.com>
2025-06-27 13:55:59 +0900

    feat: rss feed

---
+++ src/pages/posts/feed.xml.ts
@@ -0,0 +1,30 @@
+import { getCollection } from "astro:content";
+import rss from "@astrojs/rss";
+import type { APIRoute } from "astro";
+
+export const GET: APIRoute = async (context) => {
+	if (!context.site) {
+		throw new Error("Site is required.");
+	}
+
+	const publicPosts = (await getCollection("posts"))
+		.filter((post) => post.data.hide !== true)
+		.sort(
+			(a, b) =>
+				new Date(b.data.publishedDate).getTime() -
+				new Date(a.data.publishedDate).getTime(),
+		);
+
+	return rss({
+		trailingSlash: false,
+		title: "書いたもの / RSS フィード",
+		description:
+			"slimalizedが書いたもののRSSフィードです。このページのURLをRSSリーダーに登録できます。",
+		site: context.site,
+		items: publicPosts.map((post) => ({
+			title: post.data.title,
+			pubDate: post.data.publishedDate,
+			link: `/posts/${post.id}`,
+		})),
+	});
+};
